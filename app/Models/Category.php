8763d9b91d7c84303957eebf0b3629a9aef137bc
mahmoudebrahim97 <145123988+mahmoudebrahim97@users.noreply.github.com>
2023-10-24 16:16:35 +0300

    Dashboard Task

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,21 +8,6 @@ class Category extends Model
 {
     protected $table="categories";
     protected $fillable = ['name' , 'image' ,'img_path', 'category_id' , 'status','description'];
-        public function uploadOne($file, $folder)
-    {
-        $path = $file->store($folder, 'public');
-        return $path;
-    }
-    public function getImagePathAttribute()
-    {
-        return asset('categories/' . $this->image);
-    }
-
-    public function setImageAttribute($value) {
-        if ( null != $value &&  is_file($value) ) {
-            $this->attributes['image'] = $this->uploadOne($value, 'categories');
-        }
-    }
 
     public function child(){
         $this->hasMany(Category::class,'category_id');
